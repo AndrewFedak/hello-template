a5f58e850e15e42dbbf72e407c262e2ec1814ea5
Andrii_Fedak <Andrii_Fedak@epam.com>
2022-01-13 21:48:37 +0200

    bad staging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'This is super staging!!!\n'
+  const msg = 'This is bad staging!!!\n'
   res.end(msg);
 });
 
