848a0bf767d6aa55dcad17a17dd578eed3e2148b
Andrii_Fedak <Andrii_Fedak@epam.com>
2022-01-13 21:55:05 +0200

    very great bad

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'This is very bad staging!!!\n'
+  const msg = 'This is great bad staging!!!\n'
   res.end(msg);
 });
 
